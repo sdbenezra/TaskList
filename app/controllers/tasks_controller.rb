e119093d9081e890df59f2f33126a2c2d0bfc1bc
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-17 16:39:31 -0700

    Added show method to tasks controller, updated route file, added show view.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,20 @@
 class TasksController < ApplicationController
+  TASKS = [
+    {task: "Feed the dog", detail: "Use the chicken in the fridge"},
+    {task: "Take out the trash", detail: "by Tuesday"},
+    {task: "Do homework", detail: "due Thursday"},
+    {task: "Make dinner", detail: "defrost roast"}
+  ]
+
   def index
-    @tasks = ["Feed the dog", "Take out the trash", "Do homework", "Make dinner"]
+    @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @tasks = TASKS[task_id]
+    if @tasks.nil?
+      head :not_found
+    end
   end
 end

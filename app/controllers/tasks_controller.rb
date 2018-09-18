2de3dd02cd79c99c8037bc3f1adcec3da74f8be4
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-18 15:41:59 -0700

    Create Task model. Update views.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
-  TASKS = [
-    {task: "Feed the dog", detail: "Use the chicken in the fridge"},
-    {task: "Take out the trash", detail: "by Tuesday"},
-    {task: "Do homework", detail: "due Thursday"},
-    {task: "Make dinner", detail: "defrost roast"}
-  ]
+  # TASKS = [
+  #   {task: "Feed the dog", detail: "Use the chicken in the fridge"},
+  #   {task: "Take out the trash", detail: "by Tuesday"},
+  #   {task: "Do homework", detail: "due Thursday"},
+  #   {task: "Make dinner", detail: "defrost roast"}
+  # ]
+
+  TASKS = Task.all
 
   def index
     @tasks = TASKS

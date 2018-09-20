c7ed216381350fc46fc499896557f07a439c8e06
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-20 14:52:56 -0700

    Enable new task method.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,4 +19,24 @@ class TasksController < ApplicationController
       head :not_found
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    )
+
+    is_successful_save = @task.save
+
+    if is_successful_save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
 end
